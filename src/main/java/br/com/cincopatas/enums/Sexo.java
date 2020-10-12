a7de773e2fad3dcbc18d2fe46cf9c55adcd1b20e
Joao Amaral <joaovillanova@hotmail.com.br>
2020-10-12 13:11:13 -0300

    Testando branch

---
+++ src/main/java/br/com/cincopatas/enums/Sexo.java
@@ -0,0 +1,5 @@
+package br.com.cincopatas.enums;
+
+public class Sexo {
+
+}
---
+++ src/main/java/br/com/cincopatas/model/Pessoa.java
@@ -0,0 +1,8 @@
+package br.com.cincopatas.model;
+
+import java.io.Serializable;
+
+public class Pessoa  implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+}
