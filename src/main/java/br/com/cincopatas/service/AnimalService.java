2b7894e8f99f7d0ef3accbcb3901f586bbdf5381
Tathi <tathianesouza1@gmail.com>
2020-11-18 20:51:21 -0300

    Atualiza a classe AnimalService

--- src/main/java/br/com/cincopatas/service/AnimalService.java
+++ src/main/java/br/com/cincopatas/service/AnimalService.java
@@ -15,10 +15,13 @@ import br.com.cincopatas.exception.AnimalNaoEncontradodException;
 import br.com.cincopatas.filtro.AnimalFiltro;
 import br.com.cincopatas.mapper.AnimalMapper;
 import br.com.cincopatas.model.Animal;
+import br.com.cincopatas.model.Instituicao;
 import br.com.cincopatas.repository.AnimalRepository;
 import br.com.cincopatas.repository.CidadeRepository;
 import br.com.cincopatas.repository.EstadoRepository;
+import br.com.cincopatas.repository.InstituicaoRepository;
 import br.com.cincopatas.request.AnimalRequest;
+import br.com.cincopatas.security.permissoes.PatinhasSecurity;
 
 @Service
 public class AnimalService {
@@ -29,86 +32,83 @@ public class AnimalService {
 	private CidadeRepository cidadeRepository;
 	@Autowired
 	private EstadoRepository estadoRepository;
+	@Autowired 
+	private InstituicaoRepository instituicaoRepository;
+	@Autowired
+	private PatinhasSecurity patinhasSecurity;
 	@Autowired
 	private AnimalMapper animalMapper;
-	
+
 	public List<AnimalDTO> listar() {
-		List<Animal> animais = animalRepository.findAll();		
-		return animais.stream()
-				.map(ani -> animalMapper.modelToDTO(ani))
-				.collect(Collectors.toList());
+		List<Animal> animais = animalRepository.findAll();
+		return animais.stream().map(ani -> animalMapper.modelToDTO(ani)).collect(Collectors.toList());
 	}
 
 	public List<AnimalDTO> listarPorInstituicao(AnimalFiltro filtro) {
-		List<Animal> animais = animalRepository.findAll(filtro.getCidade(), 
+		List<Animal> animais = animalRepository.findAll(
+				filtro.getCidade(), 
 				filtro.getEstado(), 
 				filtro.getPorte(),
-				filtro.getSexo(),
-				filtro.getEspecie());		
-		return animais.stream()
-					  .map(ani -> animalMapper.modelToDTO(ani))
-					  .collect(Collectors.toList());
+				filtro.getSexo(), 
+				filtro.getEspecie());
+		return animais.stream().map(ani -> animalMapper.modelToDTO(ani)).collect(Collectors.toList());
 	}
-	
+
 	public AnimalDTO buscar(Long id) {
 		Optional<Animal> animal = animalRepository.findById(id);
-	
-		if(animal.isPresent()) {
+
+		if (animal.isPresent()) {
 			return animalMapper.modelToDTO(animal.get());
 		}
-		return null;	
+		return null;
 	}
-	
+
 	public List<AnimalDTO> listarPorInstituicao(Long codigo) {
-		List<Animal> animais = animalRepository.buscarPorInstituicao(codigo);		
-		return animais.stream()
-					  .map(ani -> animalMapper.modelToDTO(ani))
-					  .collect(Collectors.toList());
+		List<Animal> animais = animalRepository.buscarPorInstituicao(codigo);
+		return animais.stream().map(ani -> animalMapper.modelToDTO(ani)).collect(Collectors.toList());
 	}
-	
+
 	@Transactional
 	public AnimalDTO salvar(AnimalRequest animalRequest) {
 
 		Animal animal = animalMapper.requestToModel(animalRequest);
+		Long codigo = patinhasSecurity.getCodigo();
+		Instituicao inst = instituicaoRepository.findById(codigo).get();
 
 		if (animalRequest.getEndereco().getCidade().getEstado().getId() == null) {
 			estadoRepository.save(animalRequest.getEndereco().getCidade().getEstado());
 			cidadeRepository.save(animalRequest.getEndereco().getCidade());
 		}
-
-		animalRequest.getPersonalidades().stream().
-			forEach(personalidade -> personalidade.setAnimal(animal));
-		animalRequest.getCuidadosVet().stream()
-			.forEach(cuidados -> cuidados.setAnimal(animal));
+		animal.setInstituicao(inst);
+		animalRequest.getPersonalidades().stream().forEach(personalidade -> personalidade.setAnimal(animal));
+		animalRequest.getCuidadosVet().stream().forEach(cuidados -> cuidados.setAnimal(animal));
 		return animalMapper.modelToDTO(animalRepository.save(animal));
 	}
 
-	
 	@Transactional
 	public void remover(Long id) {
 		try {
 			animalRepository.deleteById(id);
 			animalRepository.flush();
-		
+
 		} catch (EmptyResultDataAccessException e) {
 			throw new AnimalNaoEncontradodException(id);
-		};			
+		}
+		;
 	}
 
 	@Transactional
 	public void atualizar(AnimalRequest animalRequest) {
 		Animal animal = animalMapper.requestToModel(animalRequest);
-		
+
 		if (animalRequest.getEndereco().getCidade().getEstado().getId() == null) {
 			estadoRepository.save(animalRequest.getEndereco().getCidade().getEstado());
 			cidadeRepository.save(animalRequest.getEndereco().getCidade());
 		}
 
-		animalRequest.getPersonalidades().stream()
-			.forEach(personalidade -> personalidade.setAnimal(animal));
-		
-		animalRequest.getCuidadosVet().stream()
-			.forEach(cuidados -> cuidados.setAnimal(animal));	
+		animalRequest.getPersonalidades().stream().forEach(personalidade -> personalidade.setAnimal(animal));
+
+		animalRequest.getCuidadosVet().stream().forEach(cuidados -> cuidados.setAnimal(animal));
 		animalMapper.modelToDTO(animalRepository.save(animal));
 	}
 
